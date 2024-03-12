8a86b321ee8ebeb1972c52dd8e8b4757893110bf
d.keylock@hokan.co <d.keylock@hokan.co>
2024-03-12 12:12:07 +0000

    Remove files from git

--- ScraperWeb/ScraperWeb/obj/Debug/net8.0/ScraperWeb.AssemblyInfo.cs
+++ ScraperWeb/ScraperWeb/obj/Debug/net8.0/ScraperWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScraperWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75acce5073acf1e7bf83508509ade8bfe444d7f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScraperWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScraperWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
